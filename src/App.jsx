45e4f0394df24ddca0207c3d371edc77ab6ac073
TheHerse <hersonhdez@gmail.com>
2025-12-04 17:43:47 -0600

    Update: mobile fix test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import GlitchManager from './components/GlitchManager';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import ComingSoon from './pages/ComingSoon.jsx';
 
-function App() {
+export default function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+    return () => window.removeEventListener('resize', checkMobile);
+  }, []);
+
   return (
     <BrowserRouter>
       <Navbar />
@@ -14,9 +26,8 @@ function App() {
         <Route path="/About" element={<About />} />
         <Route path="/ComingSoon" element={<ComingSoon />} />
       </Routes>
-      <GlitchManager /> {/* ADD THIS - spawns interactive glitch tabs */}
+      {/* GLITCH TABS ONLY ON DESKTOP */}
+      {!isMobile && <GlitchManager />}
     </BrowserRouter>
   );
 }
-
-export default App;
